672d76478e841f531406c47c17b1194c823b62ef
Georgii <mga@nn.ru>
2023-01-19 23:14:45 +0300

    one more

--- src/routes/contacts.jsx
+++ src/routes/contacts.jsx
@@ -1,14 +1,12 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+
+export async function loader({ params }) {
+    return getContact(params.contactId);
+}
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const contact = useLoaderData();
 
   return (
     <div id="contact">
@@ -18,7 +16,6 @@ export default function Contact() {
           src={contact.avatar || null}
         />
       </div>
-
       <div>
         <h1>
           {contact.first || contact.last ? (
