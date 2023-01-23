72c48e309238b15fa4a4fad96c29712f12e22f05
Georgii <mga@nn.ru>
2023-01-23 22:52:41 +0300

    next step

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
 import Contact, { loader as contactLoader } from './routes/contacts';
+import EditContact, {action as editAction,} from "./routes/edit"
+import { action as destroyAction } from './routes/destroy';
 
 
 const router  = createBrowserRouter([
@@ -21,6 +23,16 @@ const router  = createBrowserRouter([
         element: <Contact />,
         loader: contactLoader,
       },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+      }
     ],
   },
 ]);
